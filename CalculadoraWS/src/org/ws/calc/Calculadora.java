f778bb275abc59447aedd6bc7c4ef31c37d54302
talesxavier1 <74221635+talesxavier1@users.noreply.github.com>
2021-01-31 13:50:08 -0300

    Add files via upload

---
+++ CalculadoraWS/src/org/ws/calc/Calculadora.java
@@ -0,0 +1,20 @@
+package org.ws.calc;
+
+public class Calculadora{
+	
+	public double soma(double i, double j){
+		return (i+j);
+	}
+	
+	public double subtracao(double i, double j){
+		return (i-j);
+	}
+	
+	public double multiplicacao(double i, double j){
+		return (i*j);
+	}
+	
+	public double divisao(double i, double j){
+		return (i/j);
+	}
+}
